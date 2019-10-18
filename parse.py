58ffe050439366f0c6bac9e3c03bb06e067054ec
Koldoshbek <koldoshbektashiev@gmail.com>
2019-10-18 15:59:41 +0600

    parsed menu courses

--- parse.py
+++ parse.py
@@ -14,13 +14,6 @@ def get_all_links(html):
     return list_of_menus
 
 
-data = {
-    'frm_kullanici': '1604.01028',
-    'frm_sifre': 'Koldoshbek'
-}
-r = requests.post(url=url, data=data)
-
-
 def to_dict(all_links):
     result = {}
 
@@ -32,8 +25,56 @@ def to_dict(all_links):
     result['reference'] = all_links[5]
     result['change_password'] = all_links[6]
     result['exit'] = all_links[7]
+    get_info(result['info'])
+    get_courses(result['courses'])
+    print(result)
     return result
 
 
+def get_info(data):
+    about_info = {}
+    soup = BeautifulSoup(get_html(data), 'lxml')
+    info = soup.find('table', class_='bgc15').find_all('td')
+    about_info['st_number'] = info[2].next
+    about_info['first_name'] = info[4].next.replace('Þ', 'Ş').replace('Ý', 'I')
+    about_info['last_name'] = info[6].next.replace('Þ', 'Ş').replace('Ý', 'I')
+    about_info['region'] = info[8].next.replace('Þ', 'Ş')
+    about_info['date_of_birth'] = info[10].next
+    about_info['name_of_father'] = info[12].next.replace('Ý', 'I')
+    about_info['name_of_mother'] = info[14].next.replace('Ý', 'I')
+    return about_info
+
+
+def get_courses(data):
+    about_courses = {}
+    soup = BeautifulSoup(get_html(data), 'lxml')
+    info = soup.find('table', class_='bgc15').find_all('tr', class_='bgc15')
+    for i in info:
+        code = i.find_all('td')[0].next.replace('&nbsp', '')
+        about_courses[code] = {}
+        about_courses[code]['name'] = i.find_all('td')[1].next.replace('&nbsp', '').replace('Ý', 'I').replace('Þ',
+                                                                                                        'Ş').replace(
+            'Ð', 'Ğ').replace('\xa0', '')
+        about_courses[code]['T'] = i.find_all('td')[2].next.replace('&nbsp', '').replace('\xa0', '')
+        about_courses[code]['U'] = i.find_all('td')[3].next.replace('&nbsp', '').replace('\xa0', '')
+        about_courses[code]['credi'] = i.find_all('td')[4].next
+        about_courses[code]['theory'] = str(i.find_all('td')[5].next).replace('<br>','').replace('</br>','')
+        about_courses[code]['practice'] = str(i.find_all('td')[6].next).replace('<br>','').replace('</br>','')
+        about_courses[code]['T(30%)'] = i.find_all('td')[7].next
+        about_courses[code]['U(20%)'] = i.find_all('td')[8].next
+    return about_courses
+
+
+def get_html(url):
+    r = requests.get(url)
+    return r.text
+
+
+data = {
+    'frm_kullanici': '1604.01028',
+    'frm_sifre': 'Koldoshbek'
+}
+r = requests.post(url=url, data=data)
+
 if __name__ == '__main__':
-    print(to_dict(get_all_links(r.text)))
+    to_dict(get_all_links(r.text))
